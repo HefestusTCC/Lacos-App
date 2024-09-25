c7b253c77c7ec718449ce50720f65381a8c4e2a3
pecraxy <bemerda@hotmail.com>
2024-09-25 11:06:10 -0300

    update: Alteração de estilo e lógica na tela de cadastro

--- src/screens/Cadastro/index.js
+++ src/screens/Cadastro/index.js
@@ -7,8 +7,10 @@ import {
   Text,
   Alert,
   Image,
-  Pressable
+  Pressable,
+  Dimensions
 } from "react-native";
+
 import { Picker } from '@react-native-picker/picker';
 import axios from "axios";
 import SERVER_IP from "../../config/serverConfig";
@@ -17,52 +19,54 @@ const App = ({ navigation }) => {
   const [email, setEmail] = useState("");
   const [username, setUsername] = useState("");
   const [fullName, setFullName] = useState("");
-  const [etecName, setEtecName] = useState(""); // Estado para o nome da ETEC
   const [password, setPassword] = useState("");
-  const [course, setCourse] = useState("");
   const [selectedEtec, setSelectedEtec] = useState("Nenhuma");
   const [cursos, setCursos] = useState([]);
+  const [selectedCourse, setSelectedCourse] = useState("");
+
 
   const handleEtecChange = (etec) => {
     setSelectedEtec(etec);
-    setCursos(etecs[etec]?.cursos || []); // Atualiza o picker de cursos, se a escola for válida
+    setCursos(etecs[etec]?.cursos || []);
+    setSelectedCourse("");
   };
 
-  let etecs = {
+  const etecs = {
     "Etec da Zona Leste (Cidade A. E. Carvalho)": {
-      cursos: ["Administração", "Contabilidade", "Desenvolvimento de Sistemas", "Logística" , "Serviços Jurídicos"]
+      cursos: ["Nenhuma", "Administração", "Contabilidade", "Desenvolvimento de Sistemas", "Logística", "Serviços Jurídicos"]
     },
     "Etec de Cidade Tiradentes (Cidade Tiradentes)": {
-      cursos: ["Administração", "Farmácia", "Nutrição e Dietética", "Química", "Segurança do Trabalho"]
+      cursos: ["Nenhuma", "Administração", "Farmácia", "Nutrição e Dietética", "Química", "Segurança do Trabalho"]
     },
     "Etec de Guarulhos": {
-      cursos: ["Administração", "Desenvolvimento de Sistemas"]
+      cursos: ["Nenhuma", "Administração", "Desenvolvimento de Sistemas"]
     },
     "Etec de Guaianazes (Guaianazes)": {
-      cursos: ["Administração", "Desenvolvimento de Sistemas","Edificações","Eletrônica","Eletrotécnica","Nutrição e Dietética",]
+      cursos: ["Nenhuma", "Administração", "Desenvolvimento de Sistemas", "Edificações", "Eletrônica", "Eletrotécnica", "Nutrição e Dietética",]
     },
     "Etec de Itaquera (Cohab 2)": {
-      cursos: ["Administração", "Contabilidade","Desenvolvimento de Sistemas","Informática"]
+      cursos: ["Nenhuma", "Administração", "Contabilidade", "Desenvolvimento de Sistemas", "Informática"]
     },
     "Etec de Suzano": {
-      cursos: ["Administração", "Comércio Exterior","Contabilidade","Enfermagem","Eventos","Química","Secretariado"]
+      cursos: ["Nenhuma", "Administração", "Comércio Exterior", "Contabilidade", "Enfermagem", "Eventos", "Química", "Secretariado"]
     },
     "Etec de Tiquatira (Penha)": {
-      cursos: ["Administração", "Design Gráfico","Modelagem do Vestuário","Química"]
+      cursos: ["Nenhuma", "Administração", "Design Gráfico", "Modelagem do Vestuário", "Química"]
     },
     "Etec Itaquera II (Itaquera)": {
-      cursos: ["Administração", "Desenho de Construção Civil","Design de Interiores","Edificações","Transações Imobiliárias"]
+      cursos: ["Nenhuma", "Administração", "Desenho de Construção Civil", "Design de Interiores", "Edificações", "Transações Imobiliárias"]
     },
     "Etec Prof. Aprígio Gonzaga (Penha)": {
-      cursos: ["Administração", "Agenciamento de Viagem","Comércio Exterior","Eletromecânica","Eletrônica","Guia de Turismo","Secretariado","Segurança do Trabalho","Turismo Receptivo"]
+      cursos: ["Nenhuma", "Administração", "Agenciamento de Viagem", "Comércio Exterior", "Eletromecânica", "Eletrônica", "Guia de Turismo", "Secretariado", "Segurança do Trabalho", "Turismo Receptivo"]
     },
     "Etec São Mateus (São Mateus)": {
-      cursos: ["Administração", "Eletrônica","Nutrição e Dietética","Segurança do Trabalho"]
+      cursos: ["Nenhuma", "Administração", "Eletrônica", "Nutrição e Dietética", "Segurança do Trabalho"]
     }
   };
 
   const handleSignUp = async () => {
-    if (!email || !username || !fullName || !etecName || !password || !course) {
+    if (!email || !username || !fullName || !password || !selectedCourse || !selectedEtec || selectedCourse == 'Nenhuma') {
+      console.log(createUserData);
       Alert.alert("Por favor, preencha todos os campos.");
       return;
     }
@@ -72,8 +76,8 @@ const App = ({ navigation }) => {
       username: username,
       email: email,
       password: password,
-      school: etecName,
-      course: course,
+      school: selectedEtec,
+      course: selectedCourse,
       birthDate: "2006-06-09",
       preferences: ["Tecnologia"]
     };
@@ -86,7 +90,7 @@ const App = ({ navigation }) => {
         setEmail("");
         setUsername("");
         setFullName("");
-        setEtecName("");
+        selectedEtec("");
         setPassword("");
         setCourse("");
         navigation.navigate('Login');
@@ -105,7 +109,7 @@ const App = ({ navigation }) => {
         style={styles.divImage}
       />
       <Text style={styles.title}>
-        <Text style={styles.welcome}>Bem Vindo(a) ao</Text>{" "}
+        <Text style={styles.welcome}>Bem Vindo(a) ao </Text>
         <Text style={styles.appName}>Laços</Text>
       </Text>
 
@@ -130,31 +134,37 @@ const App = ({ navigation }) => {
       <View>
         {/* Picker de Escolas */}
         <Text>Escolha a Escola:</Text>
-        <Picker
-          selectedValue={selectedEtec}
-          onValueChange={(itemValue) => handleEtecChange(itemValue)}
-        >
-          <Picker.Item label="Nenhuma" value="Nenhuma" />
-          {Object.keys(etecs).map((etec) => (
-            <Picker.Item key={etec} label={etec} value={etec} />
-          ))}
-        </Picker>
+        <View style={styles.pickerBox}>
+          <Picker
+            style={styles.picker}
+            selectedValue={selectedEtec}
+            onValueChange={(itemValue) => handleEtecChange(itemValue)}
+          >
+            <Picker.Item label="Nenhuma" value="Nenhuma" />
+            {Object.keys(etecs).map((etec) => (
+              <Picker.Item key={etec} label={etec} value={etec} style={styles.pickerItem} />
+            ))}
+          </Picker>
+        </View>
 
         {/* Picker de Cursos */}
         {selectedEtec !== "Nenhuma" ? (
           <>
             <Text>Escolha o Curso:</Text>
-            <Picker
-              selectedValue={null} // Pode controlar o curso selecionado aqui
-              onValueChange={(itemValue) => console.log("Curso selecionado:", itemValue)}
-            >
-              {cursos.map((curso, index) => (
-                <Picker.Item key={index} label={curso} value={curso} />
-              ))}
-            </Picker>
+            <View style={styles.pickerBox}>
+              <Picker
+                style={styles.picker}
+                selectedValue={selectedCourse} // Pode controlar o curso selecionado aqui
+                onValueChange={(itemValue) => setSelectedCourse(itemValue)}
+              >
+                {cursos.map((curso, index) => (
+                  <Picker.Item key={index} label={curso} value={curso} style={styles.pickerItem} />
+                ))}
+              </Picker>
+            </View>
           </>
         ) : (
-          <Text>Selecione uma escola para ver os cursos.</Text>
+          null
         )}
       </View>
 
@@ -167,40 +177,6 @@ const App = ({ navigation }) => {
       />
 
 
-      <Picker
-        selectedValue={course}
-        style={styles.input}
-        onValueChange={(itemValue) => setCourse(itemValue)}
-      >
-        <Picker.Item label="Selecione um curso" value="" />
-        <Picker.Item label="Administração" value="Administração" />
-        <Picker.Item label="Agenciamento de Viagem" value="Agenciamento de Viagem" />
-        <Picker.Item label="Comércio Exterior" value="Comércio Exterior" />
-        <Picker.Item label="Contabilidade" value="Contabilidade" />
-        <Picker.Item label="Desenho de Construção Civil" value="Desenho de Construção Civil" />
-        <Picker.Item label="Desenvolvimento de Sistemas" value="Desenvolvimento de Sistemas" />
-        <Picker.Item label="Design de Interiores" value="Design de Interiores" />
-        <Picker.Item label="Design Gráfico" value="Design Gráfico" />
-        <Picker.Item label="Edificações" value="Edificações" />
-        <Picker.Item label="Eletrônica" value="Eletrônica" />
-        <Picker.Item label="Eletrotécnica" value="Eletrotécnica" />
-        <Picker.Item label="Enfermagem" value="Enfermagem" />
-        <Picker.Item label="Eventos" value="Eventos" />
-        <Picker.Item label="Farmácia" value="Farmácia" />
-        <Picker.Item label="Guia de Turismo" value="Guia de Turismo" />
-        <Picker.Item label="Informática" value="Informática" />
-        <Picker.Item label="Logística" value="Logística" />
-        <Picker.Item label="Mecânica" value="Mecânica" />
-        <Picker.Item label="Modelagem do Vestuário" value="Modelagem do Vestuário" />
-        <Picker.Item label="Nutrição e Dietética" value="Nutrição e Dietética" />
-        <Picker.Item label="Recursos Humanos" value="Recursos Humanos" />
-        <Picker.Item label="Secretariado" value="Secretariado" />
-        <Picker.Item label="Segurança do Trabalho" value="Segurança do Trabalho" />
-        <Picker.Item label="Serviços Jurídicos" value="Serviços Jurídicos" />
-        <Picker.Item label="Transações Imobiliárias" value="Transações Imobiliárias" />
-        <Picker.Item label="Turismo Receptivo" value="Turismo Receptivo" />
-      </Picker>
-
       <View style={styles.containerButton}>
         <Text>Já tem conta? <Pressable onPress={() => navigation.navigate('Login')} style={styles.ancora}><Text>Faça login.</Text></Pressable></Text>
       </View>
@@ -210,7 +186,7 @@ const App = ({ navigation }) => {
     </View>
   );
 };
-
+const windowWidth = Dimensions.get('window').width;
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -239,7 +215,7 @@ const styles = StyleSheet.create({
     color: "#FF6E15",
   },
   input: {
-    width: "80%",
+    width: (windowWidth * 0.8),
     height: 40,
     borderColor: "#000",
     borderWidth: 1,
@@ -247,6 +223,22 @@ const styles = StyleSheet.create({
     marginBottom: 12,
     paddingHorizontal: 8,
     color: "#000",
+  },
+  picker: {
+  },
+  pickerBox: {
+    width: (windowWidth * 0.8),
+    height: 40,
+    borderColor: "#000",
+    borderWidth: 1,
+    borderRadius: 8,
+    marginBottom: 12,
+    color: "#000",
+    
+    textAlign: 'center'
+  },
+  pickerItem: {
+    width: 40,
   },
   buttonContainer: {
     width: "60%",
